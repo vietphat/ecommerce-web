a21f01da6b2bc20881eaa4e9142773c64cad4fe8
PHAT\vietp <vietphatt1909@gmail.com>
2023-04-11 14:18:16 +0700

    Kết nối local mongodb

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,25 @@ const app = require('./app');
 // load các biến môi trường từ .env vào process.env
 dotenv.config();
 
+// Kết nối database
+mongoose
+  .connect(process.env.DB_CONNECTION_STRING)
+  .then(() => console.log('Database connected successfully'));
+
+// Chạy server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running at ${PORT} !!!`);
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+// xử lý các lỗi
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTIONS 💥, Shutting down...');
+  console.log(`Error name: `, err.name);
+  console.log(`Error message: `, err.message);
+  console.log(`Error stack: `, err.stack);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
